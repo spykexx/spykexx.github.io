ad1233a97a1c0eb0f0fbd0eeb8de06d07fc242a2
aaron olsen <xxskatexfallenxx@hotmail.com>
2020-10-12 16:16:48 -0500

    .

---
+++ services.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".fa-chevron-down").click(function(){
+        $(".nav-links").toggleClass("nav-active");
+        $("nav").toggleClass("nav-total-active");
+        $(".fa-chevron-down").toggleClass("fa-rotate");
+    });
+    $(".nav-links li a").each(function () {
+      if(window.location.href == this.href){
+        $(this).closest("a").addClass("active");
+      }
+    });
+  });
+  
+  $(document).scroll(function() {
+    
+    $(".nav-links").removeClass("nav-active");
+    $("nav").removeClass("nav-total-active");
+    $(".fa-chevron-down").removeClass("fa-rotate");
+    if ($(document).scrollTop() >= 10) {
+      // user scrolled 50 pixels or more;
+      // do stuff      
+    } 
+  });
