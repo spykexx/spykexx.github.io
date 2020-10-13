73c62ef5cdff43572a1243f04c55c61e9534e10e
Aaron Olsen <olsenaaronm@gmail.com>
2020-10-12 21:52:49 -0500

    .adding animation

--- services.js
+++ services.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
     $(".fa-chevron-down").click(function(){
         $(".nav-links").toggleClass("nav-active");
@@ -9,6 +11,11 @@ $(document).ready(function(){
         $(this).closest("a").addClass("active");
       }
     });
+    $('#descriptionMobile1').hide();
+    $('#descriptionMobile2').hide();
+    $('#descriptionMobile3').hide();
+    $('#descriptionMobile4').hide();
+    $('#description').hide();
   });
   
   $(document).scroll(function() {
@@ -24,21 +31,71 @@ $(document).ready(function(){
 
 $('li').click(function() {
   var t = $(this).attr('id');
-  window.alert(t);
-  getID(t);
+  //window.alert(t);
+  getSize(t);
+  
 })
 
+function getSize(id) {
+  if ($(window).width() < 767) {
+    descr = "Mobile";
+    getIDMobile(id);
+ }
+ else {
+    descr = "Desktop";
+    getIDDesktop(id);
+ }
+}
 
-
-function getID(id) {
+function getIDDesktop(id) {
   var id = id;
-  if ($('#description').is(':hidden')) {
-    $('#description').show();
+  var desc = $('#description');
+  
+  if (desc.is(':hidden')) {
+    desc.show();
     $("body, html").animate({
-      scrollTop: $('#description').offset().top
+      scrollTop: desc.offset().top
     }, 800);
   }else {
-    $('#description').hide();
+    desc.hide();
   }
   
+}
+
+function getIDMobile(id){
+  var id = id;
+  
+  if(id.indexOf("c1") >= 0){
+    var desc = $('#descriptionMobile1');
+    $('#descriptionMobile2').hide();
+    $('#descriptionMobile3').hide();
+    $('#descriptionMobile4').hide();
+  }
+  if(id.indexOf("c2") >= 0){
+    var desc = $('#descriptionMobile2');
+    $('#descriptionMobile1').hide();
+    $('#descriptionMobile3').hide();
+    $('#descriptionMobile4').hide();
+  }
+  if(id.indexOf("c3") >= 0){
+    var desc = $('#descriptionMobile3');
+    $('#descriptionMobile1').hide();
+    $('#descriptionMobile2').hide();
+    $('#descriptionMobile4').hide();
+  }
+  if(id.indexOf("c4") >= 0){
+    var desc = $('#descriptionMobile4');
+    $('#descriptionMobile1').hide();
+    $('#descriptionMobile2').hide();
+    $('#descriptionMobile3').hide();
+  }
+  
+  if (desc.is(':hidden')) {
+    desc.show();
+    // $("body, html").animate({
+    //   scrollTop: desc.offset().top
+    // }, 800);
+  }else {
+    desc.hide();
+  }
 }
