4ed139aeab1af83ce44a0521236c818d298924b2
Aaron Olsen <olsenaaronm@gmail.com>
2020-10-25 17:17:10 -0500

    .

--- contactform.php
+++ contactform.php
@@ -2,15 +2,16 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
+    $telenum = $_POST['telenum'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 }
 
 $mailTo = "olsena@lmtdefense.com";
 $headers = "From: olsenaaronm@gmail.com";
-$txt = "You have received a new email from ".$name.".\n\n".$message;
+$txt = "You have received a new email from ".$name.".\n\n".$message."\n\nYou can reach them at ".$mailFrom." and ".$telenum.".";
+$subject = "New inquiry from the website from ".$name
 mail($mailTo, $subject, $txt, $headers);
-header("Location: index.html");
+header("Location: http://essentialhealthdpc.com/Test/index.html");
 
 ?>
