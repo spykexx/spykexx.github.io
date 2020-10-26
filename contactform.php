31bfbf2c7605caa90dc89c1d79d55d88cabef1c6
aaron olsen <xxskatexfallenxx@hotmail.com>
2020-10-26 13:52:29 -0500

    .

--- contactform.php
+++ contactform.php
@@ -2,14 +2,15 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
+    $telenum = $_POST['phone'];
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 }
 
-$mailTo = "olsena@lmtdefense.com";
-$headers = "From: Contact@essentialhealthdpc.com";
-$txt = "You have received a new email from ".$name.".\n\n".$message;
+$subject = "New Inquiry From Website!";
+$mailTo = "olsena@lmtdefense.com, alyssa@essentialhealthdpc.com";
+$headers = "From: contact@essentialhealthdpc.com";
+$txt = "You have received a new email from ".$name.".\n\n".$message."\n\nThey Can be contacted at ".$mailFrom." or ".$telenum;
 mail($mailTo, $subject, $txt, $headers);
 header("Location: index.html");
 
